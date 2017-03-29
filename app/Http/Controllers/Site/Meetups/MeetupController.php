7d874e67f191d0da0d35c84182e5936e9dabb350
Florian Wartner <f.wartner@icloud.com>
2017-03-29 10:15:44 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/Site/Meetups/MeetupController.php
+++ app/Http/Controllers/Site/Meetups/MeetupController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Site\Meetups;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class MeetupController extends Controller
--- app/Models/Meetups/Meetup.php
+++ app/Models/Meetups/Meetup.php
@@ -2,14 +2,14 @@
 
 namespace App\Models\Meetups;
 
-use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
+use Illuminate\Database\Eloquent\Model;
 
 class Meetup extends Model
 {
     use Searchable;
 
     protected $fillable = [
-        'name', 'link', 'url_name', 'description', 'city', 'country', 'localized_country_name', 'state', 'lat', 'lon', 'member_count', 'who', 'timezone'
+        'name', 'link', 'url_name', 'description', 'city', 'country', 'localized_country_name', 'state', 'lat', 'lon', 'member_count', 'who', 'timezone',
     ];
 }
